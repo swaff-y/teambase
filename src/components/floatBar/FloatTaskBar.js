9db8f62d967b0e1394a099dc8c2eced6b61dcd66
swaff-y <kyle@swaff.id.au>
2021-03-22 17:27:12 +1100

    Float taskbar styling

--- src/components/floatBar/FloatTaskBar.js
+++ src/components/floatBar/FloatTaskBar.js
@@ -1,5 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import CloseIcon from '@material-ui/icons/Close';
+import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline';
 import "./floatbar.css";
 
 const FloatTaskBar = (props) => {
@@ -8,7 +9,7 @@ const FloatTaskBar = (props) => {
   return(
     <div className="floatbar" style={{display:props.floatStatus}}>
       <h1 className="floatbar__heading">Add Task</h1>
-      <CloseIcon onClick={props.closeFloatTaskBar} fontSize="medium" style={{position:'absolute', right: '0px', color:'#000000',marginTop: '-50px', marginRight:'20px'}}/>
+      <CloseIcon onClick={props.closeFloatTaskBar} fontSize="medium" style={{position:'absolute', right: '0px', color:'#000000',marginTop: '-100px', marginRight:'20px'}}/>
 
       <div className="floatbar__container">
         <form>
@@ -17,28 +18,44 @@ const FloatTaskBar = (props) => {
             <input id="taskName" type="text" name="taskName" placeholder="Task Name"/>
           </div>
           <div className="floatbar__row">
-            <label for="taskName">Task Name</label><br/>
-            <input id="taskName" type="text" name="taskName" placeholder="Task Name"/>
+            <label for="dueDate">Due Date</label><br/>
+            <input id="dueDate" type="text" name="dueDate"/>
           </div>
           <div className="floatbar__row">
             <div className="floatbar__col">
-              <label for="taskName">Task Name</label><br/>
-              <input id="taskName" type="text" name="taskName" placeholder="Task Name"/>
+              <label for="status">Status</label><br/>
+              <select>
+                <option></option>
+                <option>In Progress</option>
+              </select>
             </div>
             <div className="floatbar__col">
-              <label for="taskName">Task Name</label><br/>
-              <input id="taskName" type="text" name="taskName" placeholder="Task Name"/>
+              <label for="progress">Progress</label><br/>
+              <select>
+                <option></option>
+                <option>Test</option>
+              </select>
             </div>
             <div className="floatbar__col">
-              <label for="taskName">Task Name</label><br/>
-              <input id="taskName" type="text" name="taskName" placeholder="Task Name"/>
+              <label for="category">Category</label><br/>
+              <select>
+                <option></option>
+                <option>Test</option>
+              </select>
             </div>
           </div>
           <div className="floatbar__row">
+            <label for="des">Description</label><br/>
+            <textarea id="des"></textarea>
+          </div>
+          <div className="floatbar__row assignees">
+            <label for="des">Assignees</label><br/>
+            <div className="floatbar__assignees">
+            </div>
+            <AddCircleOutlineIcon fontSize="small" style={{position: "relative", marginTop:'10px', left: "280px",}}/>
           </div>
           <div className="floatbar__row">
-          </div>
-          <div className="floatbar__row">
+            <button>Save</button>
           </div>
         </form>
       </div>
