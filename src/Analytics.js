636197ad5e70c27cbee85fa74dc34fdc474c9e48
swaff-y <kyle@swaff.id.au>
2021-05-08 10:50:14 +1000

    Added helmet to analytics and team members

--- src/Analytics.js
+++ src/Analytics.js
@@ -1,9 +1,14 @@
 import React, {useEffect, useState} from 'react';
 import "./analytics.css";
+import {Helmet} from 'react-helmet'
 
 const Analytics = (props) => {
   return(
     <div className="analytics">
+    <Helmet>
+      <title>Teambase - Analytics</title>
+        <meta name="description" content="Team Members" />
+    </Helmet>
       <h1>Analytics</h1>
     </div>
   )
--- src/TeamMembers.js
+++ src/TeamMembers.js
@@ -1,9 +1,14 @@
 import React, {useEffect, useState} from 'react';
 import "./team_members.css";
+import {Helmet} from 'react-helmet'
 
 const TeamMembers = (props) => {
   return(
     <div className="team_members">
+    <Helmet>
+      <title>Teambase - Team Members</title>
+        <meta name="description" content="Team Members" />
+    </Helmet>
       <h1>Team Members</h1>
     </div>
   )
