35d72f7751737d6b51799e0fe57dc87c42b61566
swaff-y <kyle@swaff.id.au>
2021-05-26 06:56:27 +1000

    sidebar api abstraction

--- src/authUtils.js
+++ src/authUtils.js
@@ -1,7 +1,18 @@
+import api from './api';
+
 export const authHeaders = () => {
   const authHeaders = {
     Authorization: 'Bearer ' + localStorage.jwt
   }
   // console.log("authHeaders: ", authHeaders);
   return authHeaders;
+}
+
+export const projectPriorityUpdate = (project_id,priority) => {
+  return api.post(`project-priority-update`,{
+    project_id: project_id,
+    priority: priority
+  },{
+    headers: authHeaders()
+  })
 }
--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -12,7 +12,7 @@ import Button from '@material-ui/core/Button';
 import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 import { useHistory, useParams } from "react-router-dom";
 import './sidebar.css'
-import { authHeaders } from '../../authUtils';
+import { projectPriorityUpdate } from '../../authUtils';
 import api from '../../api';
 
 const Sidebar = (props) => {
@@ -41,12 +41,13 @@ const Sidebar = (props) => {
     items.splice(result.destination.index, 0, reorderedItem);
 
     for( let i = 0; i < items.length; i++ ){
-      api.post(`project-priority-update`,{
-        project_id: items[i].id,
-        priority: i+1
-      },{
-        headers: authHeaders()
-      })
+      // api.post(`project-priority-update`,{
+      //   project_id: items[i].id,
+      //   priority: i+1
+      // },{
+      //   headers: authHeaders()
+      // })
+      projectPriorityUpdate(items[i].id,i+1)
       .then(res=>{
           // console.log("The return: ",res.data, "The item: ", items[i]);
       })
